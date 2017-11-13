5e1084d2fb65686e1d1a64314f216ed265cdeb64
dayfine <difancpa@gmail.com>
2017-11-13 14:08:59 -0500

    part1 finished

--- example/index.js
+++ example/index.js
@@ -1,4 +1,19 @@
-function red (text) {
-  let color = 'ere'
-  return color
-}
+import { projectMapper, projectFilter } from 'util/helper'
+
+let projects = []
+
+let graceshopper = {/* ... */}
+let stackathon = {/* ... */}
+let capstone = {/* ... */}
+
+projects.push(graceshopper, stackathon, capstone)
+
+projectMapper(projects)
+projectFilter(projects)
+console.log(projects)
+
+const store = { cohort: '1706-flex' /* ... */ }
+
+UI = render(store)
+store.fetchProjects()
+UI = render(store)
